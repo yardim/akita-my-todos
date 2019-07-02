1c443639227aeedb15a777a07a904925c244366c
Dmitry Yaroshenko <dmitryy.yaroshenko@dataart.com>
2019-07-02 18:35:29 +0300

    markup created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { TodosComponent } from './todos/todos.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    TodosComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
